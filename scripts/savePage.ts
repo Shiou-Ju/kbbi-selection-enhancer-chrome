147d31c33402dec1d3fb8812202b1a6381e0fdf0
Bamboo <75402531+Shiou-Ju@users.noreply.github.com>
2024-01-02 10:28:29 +0800

    feat: script for test target & produced html

---
+++ scripts/savePage.ts
@@ -0,0 +1,24 @@
+// NOTE: Please follow and respect target website's rule.
+import puppeteer from 'puppeteer';
+import { promises as fs, existsSync, mkdirSync } from 'fs';
+import { format } from 'date-fns';
+
+async function savePage(): Promise<void> {
+  const folder = './scripts/output';
+  if (!existsSync(folder)) {
+    mkdirSync(folder);
+  }
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://kbbi.co.id/arti-kata/main');
+
+  const content = await page.content();
+  const timestamp = format(new Date(), 'yyyyMMdd');
+  const filePath = `${folder}/test_target_${timestamp}.html`;
+  await fs.writeFile(filePath, content);
+
+  await browser.close();
+}
+
+savePage();
