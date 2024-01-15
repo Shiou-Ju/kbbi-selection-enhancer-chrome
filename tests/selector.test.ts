d06b87dbcccc91ed643f3bd7390f44fdb81393fd
Bamboo <75402531+Shiou-Ju@users.noreply.github.com>
2024-01-15 10:58:51 +0800

    test: Refactor selector tests with `extractMeaningWordsFromInnerHtml`

--- tests/selector.test.ts
+++ tests/selector.test.ts
@@ -5,6 +5,67 @@ const DERIVATIVES_IN_ONE_SECTION = 'https://kbbi.co.id/arti-kata/ajar';
 
 const DERIVATIVES_IN_MUTIPLE_SECTIONS = 'https://kbbi.co.id/arti-kata/acara';
 
+// TODO: complexity
+function extractMeaningWordsFromInnerHtml(innerHtml: string) {
+  const bTagSeperationRegex = /<b>.*?(?=<b>|$)/gs;
+
+  const segments = innerHtml.match(bTagSeperationRegex);
+
+  const concatedSegments: string[] = [];
+  let leadingSegment = '';
+
+  // TODO: use map
+  segments!.forEach((segment, index) => {
+    const iTagWithLessThanFiveLetterRegex = /<b>.*?<\/b> <i>.{1,5}<\/i>/;
+
+    const hasPatternMatch = segment.match(iTagWithLessThanFiveLetterRegex);
+
+    const isRootWordSection = index === 0;
+
+    if (hasPatternMatch || isRootWordSection) {
+      const hasLeadingContent = !!leadingSegment;
+
+      if (hasLeadingContent) {
+        concatedSegments.push(leadingSegment);
+        leadingSegment = '';
+      }
+      concatedSegments.push(segment);
+    } else {
+      leadingSegment += segment;
+    }
+  });
+
+  const hasStillLeadingContent = !!leadingSegment;
+
+  if (hasStillLeadingContent) {
+    concatedSegments.push(leadingSegment);
+  }
+
+  if (concatedSegments.length === 0) throw new Error('no concated');
+
+  let lastMeaningfulIndex = 0;
+
+  const secondConcated: string[] = Array(concatedSegments.length).fill(null);
+
+  concatedSegments.forEach((firstConcated, currentIndex) => {
+    const derivedWordMatch = firstConcated.match(/<b>(.*?·.*?)<\/b>/);
+
+    const isStillPartOfExplanation = !derivedWordMatch || derivedWordMatch.length === 0;
+
+    if (isStillPartOfExplanation) {
+      secondConcated[lastMeaningfulIndex] += firstConcated;
+    } else {
+      lastMeaningfulIndex = currentIndex;
+
+      secondConcated[currentIndex] = firstConcated;
+    }
+  });
+
+  const secondConcatedAfterRemovedNull = secondConcated.filter((element) => element !== null);
+
+  return secondConcatedAfterRemovedNull;
+}
+
 describe('KBBI Selector Tests', () => {
   let browser: Browser;
   let page: Page;
@@ -36,43 +97,15 @@ describe('KBBI Selector Tests', () => {
   test('words from a single paragraph should be accurately separated and contain the root word', async () => {
     await page.goto(DERIVATIVES_IN_ONE_SECTION);
 
-    const htmlContent = await page.$eval(SELECTORS.EXPLANATION_SECTORS, (el: Element) => el.innerHTML);
+    const allInnerHtml = await page.$eval(SELECTORS.EXPLANATION_SECTORS, (el: Element) => el.innerHTML);
 
-    const bTagSeperationRegex = /<b>.*?(?=<b>|$)/gs;
+    const wordSegments = extractMeaningWordsFromInnerHtml(allInnerHtml);
 
-    const segments = htmlContent.match(bTagSeperationRegex);
-
-    const combinedSegments: string[] = [];
-    let previousSegment = '';
-
-    // TODO: use map
-    segments!.forEach((segment, index) => {
-      const iTagWithLessThanFiveLetterRegex = /<b>.*?<\/b> <i>.{1,5}<\/i>/;
-
-      const patternMatch = segment.match(iTagWithLessThanFiveLetterRegex);
-
-      if (patternMatch || index === 0) {
-        if (previousSegment) {
-          combinedSegments.push(previousSegment);
-          previousSegment = '';
-        }
-        combinedSegments.push(segment);
-      } else {
-        previousSegment += segment;
-      }
-    });
-
-    if (previousSegment) {
-      combinedSegments.push(previousSegment);
-    }
-
-    if (combinedSegments.length == 0) throw new Error('no combined');
-
-    combinedSegments.forEach((segment, index) => {
+    wordSegments.forEach((segment, index) => {
       const isRootWordElement = index === 0;
       if (isRootWordElement) return;
 
-      const rootWordMatch = combinedSegments[0].match(/<b>(.*?)<\/b>/);
+      const rootWordMatch = wordSegments[0].match(/<b>(.*?)<\/b>/);
 
       const derivedWordMatch = segment.match(/<b>(.*?·.*?)<\/b>/);
 
@@ -99,47 +132,17 @@ describe('KBBI Selector Tests', () => {
   test('words from multiple paragraphs should be accurately separated and contain the root word', async () => {
     await page.goto(DERIVATIVES_IN_MUTIPLE_SECTIONS);
 
-    const allHtmlContent = await page.$$eval(SELECTORS.EXPLANATION_SECTORS, (elements) =>
+    const allInnterHtml = await page.$$eval(SELECTORS.EXPLANATION_SECTORS, (elements) =>
       elements.map((el) => el.innerHTML).join('\n\n')
     );
 
-    // console.log(allHtmlContent);
+    const wordSegments = extractMeaningWordsFromInnerHtml(allInnterHtml);
 
-    const bTagSeperationRegex = /<b>.*?(?=<b>|$)/gs;
-
-    const segments = allHtmlContent.match(bTagSeperationRegex);
-
-    const combinedSegments: string[] = [];
-    let previousSegment = '';
-
-    // TODO: use map
-    segments!.forEach((segment, index) => {
-      const iTagWithLessThanFiveLetterRegex = /<b>.*?<\/b> <i>.{1,5}<\/i>/;
-
-      const patternMatch = segment.match(iTagWithLessThanFiveLetterRegex);
-
-      if (patternMatch || index === 0) {
-        if (previousSegment) {
-          combinedSegments.push(previousSegment);
-          previousSegment = '';
-        }
-        combinedSegments.push(segment);
-      } else {
-        previousSegment += segment;
-      }
-    });
-
-    if (previousSegment) {
-      combinedSegments.push(previousSegment);
-    }
-
-    if (combinedSegments.length == 0) throw new Error('no combined');
-
-    combinedSegments.forEach((segment, index) => {
+    wordSegments.forEach((segment, index) => {
       const isRootWordElement = index === 0;
       if (isRootWordElement) return;
 
-      const rootWordMatch = combinedSegments[0].match(/<b>(.*?)<\/b>/);
+      const rootWordMatch = wordSegments[0].match(/<b>(.*?)<\/b>/);
 
       const derivedWordMatch = segment.match(/<b>(.*?·.*?)<\/b>/);
 
